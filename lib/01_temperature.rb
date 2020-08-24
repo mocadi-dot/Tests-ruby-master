5de52ddb04dce9e4c2e783a529ef34a9506c8dde
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-08-24 17:12:25 +0200

    Update 01_temperature.rb

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -1,14 +1,9 @@
 #Méthode
 def ftoc(fahrenheit)
-  temperature = fahrenheit - 32
-  celsius = (temperature) *5 / 9
-  return celsius
+  (fahrenheit - 32) * 5 / 9
 end
 
 #Méthode Celsius
 def ctof(celsius)
-  temperature = celsius.to_f
-  temperature = (temperature) *9 / 5
-  fahrenheit = (temperature) + 32
-  return fahrenheit
+  (celsius.to_f) * 9 / 5 + 32
 end
